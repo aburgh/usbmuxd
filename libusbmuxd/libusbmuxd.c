53469741df75d0c138e4c3b65a3d101d94f41d82
Aaron Burghardt <aburgh@mac.com>
2009-11-05 14:53:30 -0500

    Commented out unused variables. Changed polling to use poll() instead of ppoll() if platform is macosx.

--- libusbmuxd/libusbmuxd.c
+++ libusbmuxd/libusbmuxd.c
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
 
 // usbmuxd public interface
 #include "usbmuxd.h"
@@ -137,7 +138,7 @@ static int usbmuxd_listen()
 	int sfd;
 	uint32_t res = -1;
 	struct usbmuxd_listen_request req;
-	struct usbmuxd_header hdr;
+//	struct usbmuxd_header hdr;
 
 	req.header.length = sizeof(struct usbmuxd_listen_request);
 	req.header.version = USBMUXD_PROTOCOL_VERSION;
@@ -183,13 +184,13 @@ static int usbmuxd_listen()
 int get_next_event(int sfd, usbmuxd_event_cb_t callback, void *user_data)
 {
 	int recv_len;
-	struct usbmuxd_listen_request req;
+//	struct usbmuxd_listen_request req;
 	struct usbmuxd_header hdr;
 
 	/* block until we receive something */
 	recv_len = recv_buf_timeout(sfd, &hdr, sizeof(hdr), 0, 0);
 	if (recv_len < 0) {
-		int i;
+//		int i;
 		fprintf(stderr, "DEBUG: connection closed.\n");
 		// when then usbmuxd connection fails,
 		// generate remove events for every device that

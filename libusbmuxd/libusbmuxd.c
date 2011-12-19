5ebc4b240fc920aef836f461d255830f3d9bb3db
Aaron Burghardt <aburgh@mac.com>
2011-12-19 12:20:59 -0500

    Fixed compiler warning.

--- libusbmuxd/libusbmuxd.c
+++ libusbmuxd/libusbmuxd.c
@@ -133,7 +133,7 @@ static int receive_packet(int sfd, struct usbmuxd_header *header, void **payload
 		plist_t node = plist_dict_get_item(plist, "MessageType");
 		if (plist_get_node_type(node) != PLIST_STRING) {
 			fprintf(stderr, "%s: Error getting message type from plist!\n", __func__);
-			free(plist);
+			free((void *)plist);
 			return -EBADMSG;
 		}
 		
